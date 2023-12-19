05ee65a3c4c6d811fd747a905651bce695b79395
Gautam Ramani <gautam.ramani@artoon.in>
2023-12-19 11:57:25 +0530

    add changes

--- 7taskFunction.js
+++ 7taskFunction.js
@@ -1,3 +1,24 @@
+// function bubbleSort(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[i] > arr[j]) {
+//                 temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+// let unsortedArray = [1, 2, 11, 34, 22, 67, 43, 23];
+// let sortedArray = bubbleSort(unsortedArray)
+// console.log(sortedArray);
+  
+//   // Example usage:
+//   var unsortedArray = [5, 3, 8, 4, 2];
+//   var sortedArray = bubbleSort(unsortedArray);
+//   console.log(sortedArray);
+
 // let a = {
 //     "names": "gautam"
 // }
